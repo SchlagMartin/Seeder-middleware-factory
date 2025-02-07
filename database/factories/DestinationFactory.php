b68ac356fa7cab1e9398c6b6bb5c9c7f62c7dec3
SchlagMartin <schlagm0@gmail.com>
2025-02-07 09:50:49 +0100

    asd3

--- database/factories/DestinationFactory.php
+++ database/factories/DestinationFactory.php
@@ -18,8 +18,8 @@ class DestinationFactory extends Factory
     {
         return [
             'destination' => $this->faker->name(['A1','A2','A3','A4','A5','A6','A7','A8', 'A9','A10']),
-            'price' =>$this->faker->number(),
-            'depature' => $this->date(),
+            'price' =>$this->faker->numberBetween(40000,80000),
+            'departure' => $this->faker->date(),
         ];
     }
 }
--- database/seeders/DestinationSeeder.php
+++ database/seeders/DestinationSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 use App\Models\Destination;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
