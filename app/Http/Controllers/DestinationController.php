fd743572ef5b8dfb5f4f9fd2b32effac9af367c6
SchlagMartin <schlagm0@gmail.com>
2025-02-07 09:56:21 +0100

    Final

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    //
+    public function index()
+    {
+        $destinations = Destination::all();
+        return view('destinations.index', compact('destinations'));
+    }
+
+    public function show($id)
+    {
+        $destination = Destination::find($id);
+        return view('destinations.show', compact('destination'));
+    }
 }
